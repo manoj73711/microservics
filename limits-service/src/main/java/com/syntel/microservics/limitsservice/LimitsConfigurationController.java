f0ba00dfd388cd4ec31dbed89a68c399900c5ba5
Manoj Lekkalapudi <Manoj.Lekkalapudi.consultant@nielsen.com>
2018-09-13 16:16:43 -0400

    adding spring-cloud-config-server

--- limits-service/src/main/java/com/syntel/microservics/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/syntel/microservics/limitsservice/LimitsConfigurationController.java
@@ -1,17 +1,22 @@
 package com.syntel.microservics.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.syntel.microservics.limitsservice.bean.Configuration;
 import com.syntel.microservics.limitsservice.bean.LimitConfiguration;
 
 @RestController
 public class LimitsConfigurationController {
+	
+	@Autowired
+	Configuration conf;
 
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfiguration() {
 
-		return new LimitConfiguration(1000, 1);
+		return new LimitConfiguration(conf.getMaximum(), conf.getMinimum());
 	}
 
 }
